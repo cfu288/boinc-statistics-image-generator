09171b4e5ff0388a17fbd60c5c575cc53e3c70e8
Christopher Fu <invent288cfu@gmail.com>
2022-04-22 22:58:57 -0400

    feat: add title to img, refactor

--- index.ts
+++ index.ts
@@ -17,6 +17,11 @@ const urls = [
   "https://einsteinathome.org/userw.php?id=1041241",
 ];
 
+/**
+ * Given a list of BOINC userw urls to pull from, fetch the user stats for each and return an UserStat
+ * @param urls List of urls to fetch data from
+ * @returns List of UserStat objects in descending order based on total credit per project
+ */
 async function fetchUserStats(urls: string[]): Promise<UserStat[]> {
   const xmlParser = new XMLParser({
     stopNodes: ["wml.card.p"],
@@ -40,31 +45,57 @@ async function fetchUserStats(urls: string[]): Promise<UserStat[]> {
     });
   });
 
+  cleanedData.sort((a, b) => parseInt(b.totalCred) - parseInt(a.totalCred));
+
   return cleanedData;
 }
 
+/**
+ * Given a list of user stats, render a png image of all the user's stats in descending order and save to fs.
+ * @param stats
+ */
 async function generateUserImage(stats: UserStat[]): Promise<void> {
-  const width = 300;
-  const height = 100;
+  const width = 300,
+    height = 100,
+    LEFT_MARGIN = 4,
+    TITLE_PX = 10,
+    TEXT_PX = 8,
+    VERT_SPACE_BETWEEN = 4,
+    SCALE_FACTOR = 1;
 
-  const canvas = createCanvas(width, height);
-  const context = canvas.getContext("2d");
-  canvas.width = width * 2;
-  canvas.height = height * 2;
-  context.scale(2, 2);
+  const canvas = createCanvas(width, height),
+    context = canvas.getContext("2d");
 
+  canvas.width = width * SCALE_FACTOR;
+  canvas.height = height * SCALE_FACTOR;
+  context.scale(SCALE_FACTOR, SCALE_FACTOR);
+
+  // Initialize context and fill background color
   context.fillStyle = "#000";
   context.fillRect(0, 0, width, height);
+  context.save();
+
+  // Title
+  context.fillStyle = "#fff";
+  context.font = `bold ${TITLE_PX}pt Menlo`;
+  const text = stats?.[0].username
+    ? `${stats?.[0].username}'s Stats`
+    : "Stats for user";
+  context.fillText(text, LEFT_MARGIN, TITLE_PX + VERT_SPACE_BETWEEN);
 
   for (const [index, row] of stats.entries()) {
     const text = `${row.source} ${row.totalCred}`;
-
-    const textWidth = context.measureText(text).width;
-    context.fillRect(600 - textWidth / 2 - 10, 170 - 5, textWidth + 20, 120);
     context.fillStyle = "#fff";
-    context.font = "bold 10pt Menlo";
-
-    context.fillText(text, 4, 12 + 14 * index);
+    context.font = `bold ${TEXT_PX}pt Menlo`;
+    context.fillText(
+      text,
+      LEFT_MARGIN,
+      TITLE_PX +
+        VERT_SPACE_BETWEEN +
+        VERT_SPACE_BETWEEN +
+        TEXT_PX +
+        (TEXT_PX + VERT_SPACE_BETWEEN) * index
+    );
   }
 
   const buffer = canvas.toBuffer("image/png");
